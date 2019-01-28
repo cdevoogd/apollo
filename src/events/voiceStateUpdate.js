814deb4b50fb5d72aafb930d6f161c5f7953f2a4
cdevoogd <camdendevoogd@gmail.com>
2019-01-28 14:33:46 -0800

    Revert "Updated voiceStateUpdate to use a class"
    This reverts commit d954ebf1d0c7b2c845eadce999114d99bd29ec0e.

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,137 +1,151 @@
 /**
  * Event Handler - Voice State Update
- *
+ * 
  * This event manages the dynamic channel functionality. When a use joins a channel inside of a category marked as dynamic, the bot will
  * create a new channel, making sure that there is always one open channel in the category. The bot will also delete channels to keep only one
  * channel open.
  */
 
-module.exports.run = async (config, dynamicInfo, userOld, userNew) => {
-  const dynamicConfig = await dynamicInfo;
-  const voiceUpdate = new DynamicChannelEvent(config, dynamicConfig, userOld, userNew);
-  await voiceUpdate.manageAfter();
-  voiceUpdate.manageBefore();
-};
+module.exports.run = (config, dynamicInfo, userOld, userNew) => {
+  const channelBefore = userOld.voiceChannel;
+  const channelAfter = userNew.voiceChannel;
+  const roleEveryone = userNew.guild.defaultRole;
+  
+  let dynamicCategories;
+  let dynamicObj;
+  let currentCategory;
+  let extraChannelCount;
 
-class DynamicChannelEvent {
-  constructor(config, dynamicConfig, memberBefore, memberAfter) {
-    this.config = config;
-    this.dynamicConfig = dynamicConfig;
-    this.memberBefore = memberBefore;
-    this.memberAfter = memberAfter;
+  // Using then() because dynamicInfo is a promise.
+  dynamicInfo.then((obj) => {
+    dynamicCategories = Object.keys(obj);
+    dynamicObj = obj;
 
-    this.channelBefore = memberBefore.voiceChannel;
-    this.channelAfter = memberAfter.voiceChannel;
-    this.roleEveryone = memberAfter.guild.defaultRole;
-    this.extraChannelCount;
-    this.currentCategory;
-  }
+    // Runs when a user joins a channel
+    if (channelAfter && isDynamicChannel(channelAfter)) {
+      currentCategory = channelAfter.parentID;  
 
-  /**
-   * @function manageAfter
-   * Runs when a member joins a channel. 
-   */
-  async manageAfter() {
-    if (this.channelAfter && this.isDynamicChannel(this.channelAfter)) {
-      this.currentCategory = this.channelAfter.parentID;
-
-      if (this.isNthChannelMember(this.channelAfter, 1)) {
-        await this.manageExtraChannels(this.memberAfter);
-        this.createDynamicChannel(this.memberAfter, this.currentCategory);
+      // Checks if the user is the first person to join the channel.
+      if (isNthChannelMember(channelAfter, 1)) {
+        extraChannelCount = manageExtraChannels(userNew);
+        createDynamicChannel(userNew, extraChannelCount, currentCategory);
       }
     }
 
-
-  }
-
-  /**
-   * @function manageBefore
-   * Runs when a member leaves a channel.
-   */
-  async manageBefore() {
-    if (this.channelBefore && this.isDynamicChannel(this.channelBefore)) {
-      this.currentCategory = this.channelBefore.parentID;
-
-      if (this.isNthChannelMember(this.channelBefore, 0)) {
-        await this.manageExtraChannels(this.memberBefore);
-        this.createDynamicChannel(this.memberBefore, this.currentCategory);
+    // Runs when a user leaves a channel
+    if (channelBefore && isDynamicChannel(channelBefore)) {
+      currentCategory = channelBefore.parentID;
+      
+      // Checks if the user was the last person in the channel, making it now empty.
+      if (isNthChannelMember(channelBefore, 0)) {
+        extraChannelCount = manageExtraChannels(userOld);
+        createDynamicChannel(userOld, extraChannelCount, currentCategory);
       }
     }
-
-
-  }
-
-
+  });
 
   /**
    * @function isDynamicChannel
+   * Checks the VoiceChannel's parent ID against the list of dynamic category IDs
    * @param {VoiceChannel} channel
-   * @returns {Boolean} 
+   * @returns {Boolean}
    */
-  isDynamicChannel(channel) {
-    return this.dynamicConfig.hasOwnProperty(channel.parentID);
+  function isDynamicChannel(channel) {
+    return dynamicCategories.includes(channel.parentID);
   }
 
   /**
    * @function isNthChannelMember
+   * Checks if the user is the Nth member of the provided voice channel.
    * @param {VoiceChannel} channel 
    * @param {Integer} n 
-   * @returns {Boolean}
+   * @returns {undefined}
    */
-  isNthChannelMember(channel, n) {
-    return (channel.members.size === n) ? true : false;
+  function isNthChannelMember(channel, n) {
+    if (channel.members.array().length === n) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /**
    * @function manageExtraChannels
-   * @param {GuildMember} member
-   * @returns {undefined} 
+   * Loops through the channels in the guild to make sure that there is at least one empty dynamic channel. 
+   * If there is more than one empty channel, it will be deleted.
+   * @param {GuildMember} guildMember 
+   * @returns {Integer}
    */
-  manageExtraChannels(member) {
-    const channels = member.guild.channels.array();
-    this.extraChannelCount = 0;
-
+  function manageExtraChannels(guildMember) {
+    // Manages the extra dynamic channels in the server. 
+    // Checks if there is at least one empty, extra channel. Channels after that will be deleted.
+    let channels = guildMember.guild.channels.array();
+    let extraChannels = 0;
     for (let channel of channels) {
-      if (channel.parentID === this.currentCategory
-        && channel.type === 'voice'
-        && channel.members.array().length === 0) {
-        if (this.extraChannelCount === 1) {
-          // Deletes all but one empty channel.
+      // Check if the channel is dynamic, a voice channel, and is empty
+      if (channel.parentID === currentCategory && channel.type === 'voice' && channel.members.array().length === 0) {
+        if (extraChannels === 1) {
+          // If at least one empty dynamic channel has already been found, delete the extras
           channel.delete();
         } else {
-          this.unlockLockedChannel(channel);
-          this.extraChannelCount = 1;
+          // Check if the empty extra channel is locked, and if so unlock it.
+          if (isChannelLocked(channel)) {
+            unlockChannel(channel);
+          }
+          // Set that there is at least one empty dynamic channel
+          extraChannels = 1;
         }
-
       }
     }
-  }
 
-  /**
-   * @function unlockLockedChannel
-   * @param {VoiceChannel} channel
-   * @returns {undefined} 
-   */
-  unlockLockedChannel(channel) {
-    // Locked channels deny the CONNECT permissions to @everyone (excluding admin role)
-    if (!channel.permissionsFor(this.roleEveryone).toArray().includes('CONNECT')) {
-      channel.overwritePermissions(this.roleEveryone, { CONNECT: null });
-      channel.overwritePermissions(this.config.adminRoleID, { CONNECT: null });
+    return extraChannels;
+
+    /**
+     * @function isChannelLocked
+     * Checks if the extra voice channel is locked by checking if the everyone role has the CONNECT permission.
+     * @param {VoiceChannel} currentChannel 
+     * @returns {Boolean}
+     */
+    function isChannelLocked(currentChannel) {
+      // Checks if @everyone is able to connect to the channel. If not, it must be locked.
+      const permissions = currentChannel.permissionsFor(roleEveryone).toArray();
+      return !permissions.includes('CONNECT');
+    }
+
+    /**
+     * @function unlockChannel
+     * Unlocks the voice channel by setting the locked/overwritten permissions to default.
+     * @param {VoiceChannel} currentChannel 
+     * @returns {undefined}
+     */
+    function unlockChannel(currentChannel) {
+      currentChannel.overwritePermissions(roleEveryone, { CONNECT: null });
+      currentChannel.overwritePermissions(config.adminRoleID, { CONNECT: null });
     }
   }
 
   /**
+   * @async
    * @function createDynamicChannel
-   * @param {GuildMember} member 
-   * @returns {undefined}
+   * Creates a new dynamic channel under the current category.
+   * @param {GuildMember} guildMember 
+   * @param {Integer} extraChannels 
+   * @param {String} currentCat 
    */
-  async createDynamicChannel(member, category) {
-    if (this.extraChannelCount === 0) {
-      // On creation, the channel is hidden while it is moved to its category, where it is then unhidden. The server owner will still be able to see this movement.
-      member.guild.createChannel(this.dynamicConfig[category], 'voice', [{ id: this.roleEveryone, denied: ['VIEW_CHANNEL'] }])
-        .then(channel => channel.setParent(category))
-        .then(channel => channel.overwritePermissions(this.roleEveryone, { VIEW_CHANNEL: true }))
-        .catch(err => console.error(err));
+  async function createDynamicChannel(guildMember, extraChannels, currentCat) {
+    // Using currentCat as a parameter to keep it from placing channels in the wrong category when running quickly.
+    // Permissions on channel creation to hide channel before it is moved
+    const creationPermissions = [{
+      id: roleEveryone, denied: ['VIEW_CHANNEL']
+    }];
+
+    if (extraChannels === 0) {
+      await guildMember.guild.createChannel(dynamicObj[currentCat], 'voice', creationPermissions)
+        // Move the channel to the current dynamic category
+        .then(channel => channel.setParent(currentCat))
+        // Allow it to be viewed again
+        .then(channel => channel.overwritePermissions(roleEveryone, {VIEW_CHANNEL: true}))
+        .catch(console.error);
     }
   }
-}
+};
