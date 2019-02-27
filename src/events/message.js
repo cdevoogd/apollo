6c73c44846be7b439c7e7c27548881ae2b5ce89d
cdevoogd <camdendevoogd@gmail.com>
2019-02-26 16:20:38 -0800

    Add 'message' event

--- src/events/message.js
+++ src/events/message.js
@@ -5,6 +5,21 @@
  * This module checks messages for commands, and redirects them to be processed if found.
  */
 
-module.exports.process = function () {
-  //
+const commands = require('../commands');
+const config = require('../config');
+
+module.exports.process = function (client, message) {
+  const messageContent = message.content.split(' ');
+  const keyword = messageContent[0];
+
+  if (message.author === client.user) { return; }
+  if (message.channel.type !== 'text') { return; }
+
+  if (keyword.startsWith(config.prefix)) {
+    const command = keyword.slice(1).toLowerCase();
+    if (commands[command] && config.commands[command].enabled) {
+      commands[command].exec(message);
+    }
+  }
+
 };
