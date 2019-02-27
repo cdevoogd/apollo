8bc5e5402505f427835effd0b3e097ce72c2c5a7
cdevoogd <camdendevoogd@gmail.com>
2019-02-26 18:04:38 -0800

    Update 'message' event to check for custom chat commands

--- src/events/message.js
+++ src/events/message.js
@@ -5,6 +5,7 @@
  * This module checks messages for commands, and redirects them to be processed if found.
  */
 
+const cache = require('../internal/cache');
 const commands = require('../commands');
 const config = require('../config');
 
@@ -15,6 +16,7 @@ module.exports.process = function (client, message) {
   if (message.author === client.user) { return; }
   if (message.channel.type !== 'text') { return; }
 
+  // Check for built-in command & run if valid
   if (keyword.startsWith(config.prefix)) {
     const command = keyword.slice(1).toLowerCase();
     if (commands[command] && config.commands[command].enabled) {
@@ -22,4 +24,15 @@ module.exports.process = function (client, message) {
     }
   }
 
+  checkForCustomCommands(message);
+};
+
+const checkForCustomCommands = async function (message) {
+  const customCommands = await cache.getCommands();
+  for (const word of message.content.split(' ')) {
+    if (customCommands[word]) {
+      message.channel.send(customCommands[word]);
+      break;
+    }
+  }
 };
