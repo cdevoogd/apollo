6b69a3ba0dcbb44102c00113daa8fcf302c81e40
Camden DeVoogd <camdendevoogd@gmail.com>
2021-07-08 22:14:27 -0700

    Revert the repo to a clean state for rewrite

--- src/core/cache.js
+++
@@ -1,9 +0,0 @@
-const database = require('../database');
-
-let commands = database.getCommands();
-let dynamicConfigs = database.getDynamicConfigs();
-
-module.exports.getCommands = () => commands;
-module.exports.getDynamicConfigs = () => dynamicConfigs;
-module.exports.cacheCommands = () => commands = database.getCommands();
-module.exports.cacheDynamicConfigs = () => dynamicConfigs = database.getDynamicConfigs();
