54d6980f8e2ded1b4fdae7906afe5f88c90c26f9
Camden DeVoogd <camdendevoogd@gmail.com>
2022-01-22 18:05:51 -0800

    Add bot login and ready handling
    The token is currently being read in from environment variables. In the
    future, when the bot is deployed in Docker, this may be updated to use
    another method, or I may use an env file with docker-compose. For now,
    it works just fine.

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/cdevoogd/apollo
+
+go 1.17
+
+require (
+	github.com/bwmarrin/discordgo v0.23.3-0.20211228023845-29269347e820 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.8.1
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/cdevoogd/apollo/internal/event"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+func run() error {
+	token, set := os.LookupEnv("DISCORD_API_TOKEN")
+	if !set {
+		return errors.New("DISCORD_API_TOKEN is not set")
+	}
+
+	session, err := discordgo.New("Bot " + token)
+	if err != nil {
+		return errors.Wrap(err, "unable to create session")
+	}
+
+	session.AddHandler(event.HandleReady)
+
+	err = session.Open()
+	if err != nil {
+		return errors.Wrap(err, "error opening websocket to discord")
+	}
+	defer session.Close()
+
+	// Wait here until CTRL-C or other term signal is received.
+	kill := make(chan os.Signal, 1)
+	signal.Notify(kill, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-kill
+
+	logrus.Warn("Shutting down Apollo...")
+	return nil
+}
